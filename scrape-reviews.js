d8a40ade9f107949d1e70c8b51450d3902101a1f
Arron Linton <arronlinton@Arrons-MBP.home>
2017-08-11 09:08:38 -0400

    scrape edits

--- scrape-reviews.js
+++ scrape-reviews.js
@@ -21,6 +21,7 @@ function scrape(){
       })
       // .goto(urlOb.url)
       .goto("https://www.coursereport.com/schools/general-assembly#/reviews")
+      .wait(10000)
       .evaluate(function() {
           return document.body.innerHTML;
       }).end().then(function(html) {
@@ -35,74 +36,61 @@ function scrape(){
         var $ = cheerio.load(html);
 
         var entry = $(".review");
-        console.log(entry.length);
-        // console.log("oh yes", review[0].childrsen)
+        var text = $(".ratings").children("span", ":before");
 
-
-        // for (var i = 0; i < review.length; i++) { 
-        //     $(review[i])
-        //     console.log("oh damn ", i, review[i])
-        // }
-
-        // var text = $(".ratings span");
-
-        // console.log("lovely", text[0])
-
+        // console.log("lovely", text[0].children[0])
 
 
     entry.each(function(reviewId, reviewEntry){
 
       var review = $(this).children("div");
-      // console.log("review", review[2].children[0].data)
-  // ======== REVIEW DATE ======== //
-
+    
+  // ========== REVIEW DATE ========== //
       // children[2] gives us the object inside review that holds review-date information
       var reviewDate = review[0].children[2].children[0].data;
       console.log("review date", reviewDate);
 
       // children[4] is associated with the .ratings class that holds rating information
-      var ratingCategories = review[0].children[4].children.slice(1);
-      // console.log("test", ratingCategories)
+      // each .rating class has 3 rows and we skip over the 1st row 
+      var rating = review[0].children[4].children.slice(1);
 
-      // console.log("categories", ratingCategories.length)
+      // console.log("categories", rating.length)
 
-      for (var i = 0; i < ratingCategories.length; i++) {
-          console.log("category", ratingCategories[i].children[0].children[0].data);
-          console.log("category", ratingCategories[i].children[2].children[0].data);
+      for (var i = 0; i < rating.length; i++) {
 
+
+  // ========== RATING CATEGORIES ========== //
+  // First row inside .ratings
+  // category label for overall exp. and intructors
+        var category = rating[i].children[0].children[0].data;
+  // Second row inside .ratings
+  // category label for cirrculum and job assistance
+        var category1 = rating[i].children[2].children[0].data;
+
+        var star = rating[i].children[1];
+        var star1 = rating[i].children[3];
+
+          // .children("span icon-full_star", ":before"));
+  
+
+          // console.log("category", category)
+          // console.log("stars baby", star)
+          console.log("category", category1)
+          console.log("stars baby", star1.children[0]);
+
+  //    console.log("stars", rating[i].children[1].children[0].children[0]);
+  // console.log("category", rating[i].children[2].children[0].data);
 
       }
 
-        // ratingCategories.each(function(i, category){
-        //   // var category = $(category).children[i].children[0].data;
-        //   console.log("category", category)
+      // var stars = $(this).children("span .icon-full_star", ":before") 
+      // console.log("stars", stars.children(".icon-full_star").);
 
-        // })
-      // console.log("review", ratingCategories.children[0].children[0].data);
+
+
       console.log("-------------------------------");
 
-      	// var stars = $(".ratings text-right");
-      	// console.log("stars", stars)
-
-        // var tableRows = $(".ratings")[0]
-        // console.log("ratingInfo", tableRows)
-        // console.log("1st row", tableRows);
-        // tableRows.each(function(i, element) {
- 
-        	// var tds = $(element).children();
-        	// console.log("id", i)
-        	// tableRows.each(function(tdIndex, td){
-        		// var val = $(this);
-        		// console.log("this", val); 
-
-  // ======== RATING CATEGORIES ======== //
-        		// var rating = $(this).text().trim();
-        		// var stars = $(".ratings row")[0]
-        		// console.log("rating", rating)
-        		// console.log("stars", stars)
-        	// });
-        // });
-       });
+    });
 
         //skip first row
         // tableRows.slice(1).each(function(i, element) {
