0d33b9af5aebdb7c77745058c5728236b7106bdf
Ahmed Haque <ahaque@trilogyed.com>
2017-09-02 04:25:23 -0400

    Added word count

--- scrape-reviews.js
+++ scrape-reviews.js
@@ -10,209 +10,209 @@ var schedule = require("node-schedule")
 
   // Included Schools
   var data = [
-      // { 'school': 'General Assembly',
-      //   'url': 'https://www.coursereport.com/schools/general-assembly',
-      //   'category': 'GENERAL',
-      //   'trilogy': 'NON-TRILOGY'
-      // },
-      // {
-      //   "school": "Bloc",
-      //   "url": "https://www.coursereport.com/schools/bloc",
-      //   "category": "BLOC",
-      //   "trilogy": "NON-TRILOGY"
-      // },
-      // {
-      //   "school": "App Academy",
-      //   "url": "https://www.coursereport.com/schools/app-academy",
-      //   "category": "APP ACADEMY",
-      //   "trilogy": "NON-TRILOGY"
-      // },
-      // {
-      //   "school": "Le Wagon",
-      //   "url": "https://www.coursereport.com/schools/le-wagon",
-      //   "category": "LE WAGON",
-      //   "trilogy": "NON-TRILOGY"
-      // },
-      // {
-      //   "school": "Dev Mountain",
-      //   "url": "https://www.coursereport.com/schools/devmountain",
-      //   "category": "DEV MOUNTAIN",
-      //   "trilogy": "NON-TRILOGY"
-      // },
-      // {
-      //   "school": "Iron Hack",
-      //   "url": "https://www.coursereport.com/schools/ironhack",
-      //   "category": "IRON HACK",
-      //   "trilogy": "NON-TRILOGY"
-      // },
-      // {
-      //   "school": "Iron Yard",
-      //   "url": "https://www.coursereport.com/schools/the-iron-yard",
-      //   "category": "IRON YARD",
-      //   "trilogy": "NON-TRILOGY"
-      // },
-      // {
-      //   "school": "Dev Bootcamp",
-      //   "url": "https://www.coursereport.com/schools/dev-bootcamp",
-      //   "category": "DEV BOOTCAMP",
-      //   "trilogy": "NON-TRILOGY"
-      // },
-      // {
-      //   'school': 'Hack Reactor',
-      //   'url': 'https://www.coursereport.com/schools/hack-reactor',
-      //   'category': 'HACKREACTOR',
-      //   'trilogy': 'NON-TRILOGY'
-      // },
-      // {
-      //   'school': 'Galvanize',
-      //   'url': 'https://www.coursereport.com/schools/galvanize',
-      //   'category': 'GALVANIZE',
-      //   'trilogy': 'NON-TRILOGY'
-      // },
-      // {
-      //   'school': 'Flat-Iron',
-      //   'url': 'https://www.coursereport.com/schools/flatiron-school',
-      //   'category': 'FLATIRON',
-      //   'trilogy': 'NON-TRILOGY'
-      // },
-      // {
-      //   'school': 'Fullstack Academy',
-      //   'url': 'https://www.coursereport.com/schools/fullstack-academy',
-      //   'category': 'FULLSTACK ACADEMY',
-      //   'trilogy': 'NON-TRILOGY'
-      // },
-      // {
-      //   'school': 'New York Code + Design Academy',
-      //   'url': 'https://www.coursereport.com/schools/new-york-code-design-academy',
-      //   'category': 'NYCDA',
-      //   'trilogy': 'NON-TRILOGY'
-      // },
-      // {
-      //   'school': 'Grace Hopper Program',
-      //   'url': 'https://www.coursereport.com/schools/the-grace-hopper-program',
-      //   'category': 'GRACE HOPPER',
-      //   'trilogy': 'NON-TRILOGY'
-      // },
+      { 'school': 'General Assembly',
+        'url': 'https://www.coursereport.com/schools/general-assembly',
+        'category': 'GENERAL',
+        'trilogy': 'NON-TRILOGY'
+      },
+      {
+        "school": "Bloc",
+        "url": "https://www.coursereport.com/schools/bloc",
+        "category": "BLOC",
+        "trilogy": "NON-TRILOGY"
+      },
+      {
+        "school": "App Academy",
+        "url": "https://www.coursereport.com/schools/app-academy",
+        "category": "APP ACADEMY",
+        "trilogy": "NON-TRILOGY"
+      },
+      {
+        "school": "Le Wagon",
+        "url": "https://www.coursereport.com/schools/le-wagon",
+        "category": "LE WAGON",
+        "trilogy": "NON-TRILOGY"
+      },
+      {
+        "school": "Dev Mountain",
+        "url": "https://www.coursereport.com/schools/devmountain",
+        "category": "DEV MOUNTAIN",
+        "trilogy": "NON-TRILOGY"
+      },
+      {
+        "school": "Iron Hack",
+        "url": "https://www.coursereport.com/schools/ironhack",
+        "category": "IRON HACK",
+        "trilogy": "NON-TRILOGY"
+      },
+      {
+        "school": "Iron Yard",
+        "url": "https://www.coursereport.com/schools/the-iron-yard",
+        "category": "IRON YARD",
+        "trilogy": "NON-TRILOGY"
+      },
+      {
+        "school": "Dev Bootcamp",
+        "url": "https://www.coursereport.com/schools/dev-bootcamp",
+        "category": "DEV BOOTCAMP",
+        "trilogy": "NON-TRILOGY"
+      },
+      {
+        'school': 'Hack Reactor',
+        'url': 'https://www.coursereport.com/schools/hack-reactor',
+        'category': 'HACKREACTOR',
+        'trilogy': 'NON-TRILOGY'
+      },
+      {
+        'school': 'Galvanize',
+        'url': 'https://www.coursereport.com/schools/galvanize',
+        'category': 'GALVANIZE',
+        'trilogy': 'NON-TRILOGY'
+      },
+      {
+        'school': 'Flat-Iron',
+        'url': 'https://www.coursereport.com/schools/flatiron-school',
+        'category': 'FLATIRON',
+        'trilogy': 'NON-TRILOGY'
+      },
+      {
+        'school': 'Fullstack Academy',
+        'url': 'https://www.coursereport.com/schools/fullstack-academy',
+        'category': 'FULLSTACK ACADEMY',
+        'trilogy': 'NON-TRILOGY'
+      },
+      {
+        'school': 'New York Code + Design Academy',
+        'url': 'https://www.coursereport.com/schools/new-york-code-design-academy',
+        'category': 'NYCDA',
+        'trilogy': 'NON-TRILOGY'
+      },
+      {
+        'school': 'Grace Hopper Program',
+        'url': 'https://www.coursereport.com/schools/the-grace-hopper-program',
+        'category': 'GRACE HOPPER',
+        'trilogy': 'NON-TRILOGY'
+      },
       {
         'school': 'Rutgers',
         'url': 'https://www.coursereport.com/schools/rutgers-bootcamps',
         'category': 'RUTGERS',
         'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'UCF Orlando',
+        'url': 'https://www.coursereport.com/schools/ucf-coding-boot-camp',
+        'category': 'UCF',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'UCLA',
+        'url': 'https://www.coursereport.com/schools/the-coding-boot-camp-at-ucla-extension',
+        'category': 'UCLA',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'UNC Chapel Hill',
+        'url': 'https://www.coursereport.com/schools/the-coding-boot-camp-at-unc-chapel-hill',
+        'category': 'UNC Chapel Hill',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'UNC Charlotte',
+        'url': 'https://www.coursereport.com/schools/the-coding-boot-camp-at-unc-charlotte',
+        'category': 'UNC Charlotte',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'UC San Diego',
+        'url': 'https://www.coursereport.com/schools/the-coding-boot-camp-at-uc-san-diego-extension',
+        'category': 'UCSD',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'UT Austin',
+        'url': 'https://www.coursereport.com/schools/UT-Austin-Boot-Camps',
+        'category': 'UT AUSTIN',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'UCIrvine',
+        'url': 'https://www.coursereport.com/schools/UC-Irvine-Boot-Camps',
+        'category': 'UCIRVINE',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'UC Berkeley',
+        'url': 'https://www.coursereport.com/schools/berkeley-boot-camps',
+        'category': 'UC Berkeley',
+        'trilogy': 'TRILOGY'
+      },
+       {
+        'school': 'UT Austin',
+        'url': 'https://www.coursereport.com/schools/UT-Austin-Boot-Camps',
+        'category': 'UT Austin',
+        'trilogy': 'TRILOGY'
+      },
+       {
+        'school': 'SMU',
+        'url': 'https://www.coursereport.com/schools/SMU-coding-boot-camp',
+        'category': 'SMU',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'University of Utah',
+        'url': 'https://www.coursereport.com/schools/university-of-utah-professional-education-coding-boot-camp',
+        'category': 'University of Utah',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'University of Kansas',
+        'url': 'https://www.coursereport.com/schools/university-of-kansas-coding-boot-camp',
+        'category': 'University of Kansas',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'Northwestern',
+        'url': 'https://www.coursereport.com/schools/northwestern-coding-boot-camps',
+        'category': 'Northwestern',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'Georgia Tech',
+        'url': 'https://www.coursereport.com/schools/georgia-tech-coding-boot-camp',
+        'category': 'Georgia Tech',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'Case Western',
+        'url': 'https://www.coursereport.com/schools/case-western-reserve-university-coding-boot-camp',
+        'category': 'Case Western',
+        'trilogy': 'TRILOGY'
+      },
+       {
+        'school': 'University of Denver',
+        'url': 'https://www.coursereport.com/schools/university-of-denver-coding-boot-camp',
+        'category': 'Denver',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'GWU',
+        'url': 'https://www.coursereport.com/schools/gw-boot-camps',
+        'category': 'GWU',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'Arizona',
+        'url': 'https://www.coursereport.com/schools/university-of-arizona-coding-boot-camp',
+        'category': 'Arizona',
+        'trilogy': 'TRILOGY'
+      },
+      {
+        'school': 'Thinkful',
+        'url': 'https://www.coursereport.com/schools/thinkful',
+        'category': 'THINKFUL',
+        'trilogy': 'NON-TRILOGY'
       }
-      // {
-      //   'school': 'UCF Orlando',
-      //   'url': 'https://www.coursereport.com/schools/ucf-coding-boot-camp',
-      //   'category': 'UCF',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'UCLA',
-      //   'url': 'https://www.coursereport.com/schools/the-coding-boot-camp-at-ucla-extension',
-      //   'category': 'UCLA',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'UNC Chapel Hill',
-      //   'url': 'https://www.coursereport.com/schools/the-coding-boot-camp-at-unc-chapel-hill',
-      //   'category': 'UNC Chapel Hill',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'UNC Charlotte',
-      //   'url': 'https://www.coursereport.com/schools/the-coding-boot-camp-at-unc-charlotte',
-      //   'category': 'UNC Charlotte',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'UC San Diego',
-      //   'url': 'https://www.coursereport.com/schools/the-coding-boot-camp-at-uc-san-diego-extension',
-      //   'category': 'UCSD',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'UT Austin',
-      //   'url': 'https://www.coursereport.com/schools/UT-Austin-Boot-Camps',
-      //   'category': 'UT AUSTIN',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'UCIrvine',
-      //   'url': 'https://www.coursereport.com/schools/UC-Irvine-Boot-Camps',
-      //   'category': 'UCIRVINE',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'UC Berkeley',
-      //   'url': 'https://www.coursereport.com/schools/berkeley-boot-camps',
-      //   'category': 'UC Berkeley',
-      //   'trilogy': 'TRILOGY'
-      // },
-      //  {
-      //   'school': 'UT Austin',
-      //   'url': 'https://www.coursereport.com/schools/UT-Austin-Boot-Camps',
-      //   'category': 'UT Austin',
-      //   'trilogy': 'TRILOGY'
-      // },
-      //  {
-      //   'school': 'SMU',
-      //   'url': 'https://www.coursereport.com/schools/SMU-coding-boot-camp',
-      //   'category': 'SMU',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'University of Utah',
-      //   'url': 'https://www.coursereport.com/schools/university-of-utah-professional-education-coding-boot-camp',
-      //   'category': 'University of Utah',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'University of Kansas',
-      //   'url': 'https://www.coursereport.com/schools/university-of-kansas-coding-boot-camp',
-      //   'category': 'University of Kansas',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'Northwestern',
-      //   'url': 'https://www.coursereport.com/schools/northwestern-coding-boot-camps',
-      //   'category': 'Northwestern',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'Georgia Tech',
-      //   'url': 'https://www.coursereport.com/schools/georgia-tech-coding-boot-camp',
-      //   'category': 'Georgia Tech',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'Case Western',
-      //   'url': 'https://www.coursereport.com/schools/case-western-reserve-university-coding-boot-camp',
-      //   'category': 'Case Western',
-      //   'trilogy': 'TRILOGY'
-      // },
-      //  {
-      //   'school': 'University of Denver',
-      //   'url': 'https://www.coursereport.com/schools/university-of-denver-coding-boot-camp',
-      //   'category': 'Denver',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'GWU',
-      //   'url': 'https://www.coursereport.com/schools/gw-boot-camps',
-      //   'category': 'GWU',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'Arizona',
-      //   'url': 'https://www.coursereport.com/schools/university-of-arizona-coding-boot-camp',
-      //   'category': 'Arizona',
-      //   'trilogy': 'TRILOGY'
-      // },
-      // {
-      //   'school': 'Thinkful',
-      //   'url': 'https://www.coursereport.com/schools/thinkful',
-      //   'category': 'THINKFUL',
-      //   'trilogy': 'NON-TRILOGY'
-      // }
     ];
 
   // Assemble Objects for Each URL (16 pages)
@@ -220,13 +220,8 @@ var schedule = require("node-schedule")
   data.forEach(function(element) {
 
     for (var i = 1; i < 16; i++){
-
       new_element = Object.assign({}, element);
-      // console.log(new_element); 
-      // console.log(new_element.school);
       new_element.url = element.url + "?page=" + i + "#/reviews"     
-      // console.log(element.url + "?page=" + i + "#/reviews")
-
       newData.push({new_element})
     }
   })
@@ -242,7 +237,7 @@ var schedule = require("node-schedule")
   };
 
 
-  var cleanRowHeadings = ['School', 'Trilogy' , 'Date', 'Course', 'Location', 'Verified', 'Overall Exp.', 'Curriculum', 'Instructors', 'Job Assistance', 'Comments'];
+  var cleanRowHeadings = ['School', 'Trilogy' , 'Date', 'Course', 'Location', 'Verified', 'Overall Exp.', 'Curriculum', 'Instructors', 'Job Assistance', 'Comments', 'Word Count'];
 
   fs.writeFile('bootcamp-reviews.csv', cleanRowHeadings + "\n" , 'utf8', function (err) {
       if (err) {
@@ -446,10 +441,12 @@ var schedule = require("node-schedule")
         // We have to replace the commas in the comment section because the csv file will interpret a comma as a separation between cells
         // console.log('comments', comments)
           comments = comments.replace(/\,/g, "")
-          // comments = comments.replace(/\t/g, '').split('\r\n');
           comments = comments.replace(/"/g, "\\'");
           comments = '"' + comments + '"'
           cleanRow.push(comments)
+
+          wordCount = comments.split(" ").length;
+          cleanRow.push(wordCount)
         }
 
         fs.appendFile('bootcamp-reviews.csv', cleanRow + "\n" , 'utf8', function (err) {
